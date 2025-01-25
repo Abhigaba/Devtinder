c0b250c820823346e42c0cd7c9c2da8b34093974
Abhigaba <gabaabi2445@gmail.com>
2025-01-26 02:37:45 +0530

    "Added feed api "

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,6 @@
 import  express, {Response, Request}  from "express";
 import { authMiddle } from "../middleware/authMiddleware";
+import dUser from '../database/dUser'
 import connectionRequest from "../database/connection";
 export const userRouter = express.Router();
 
@@ -39,7 +40,8 @@ userRouter.get('/connections', authMiddle, async(req: Request, res: Response) =>
                 {senderId: user, status: "accepted"},
                 {receiverId: user, status: "accepted"}
             ] 
-        }).populate('receiverId', 'Firstname Lastname  photoUrl age about skills').populate('senderId', 'Firstname Lastname photoUrl age about skills')
+        }).populate('receiverId', 'Firstname Lastname  photoUrl age about skills')
+        .populate('senderId', 'Firstname Lastname photoUrl age about skills')
 
         if (!getUsers) { 
             throw new Error('No Connections Yet');
@@ -47,7 +49,6 @@ userRouter.get('/connections', authMiddle, async(req: Request, res: Response) =>
 
         const id = user.toString()
      
-
         const data = getUsers.map((n) => {
             if (n.senderId.toString() === id) {
                 return n.receiverId
@@ -61,5 +62,44 @@ userRouter.get('/connections', authMiddle, async(req: Request, res: Response) =>
     }
     catch(error: any) { 
         res.status(400).send(error.message) ;
+    }
+})
+
+userRouter.get('/feed', authMiddle, async (req: Request, res: Response) => {
+
+    try { 
+
+        const page = parseInt(req.query.page as string ) || 1 ;
+        const limit = parseInt(req.query.limit as string) || 10 
+
+
+        const skip = (page - 1)*limit
+        const user = req.user ;
+        const seenUsers = await connectionRequest.find({
+        $or : [
+            {senderId : user._id},
+            {receiverId: user.__id} ]
+        }).select('senderId receiverId')
+
+        const seen = new Set() 
+        seenUsers.forEach((n) => {
+            seen.add(n.senderId.toString())
+            seen.add(n.receiverId.toString())
+        })
+
+        const feed = await dUser.find({
+            _id : {$nin : Array.from(seen)}
+        })
+        .select('Firstname Lastname photoUrl age about skills _id')
+        .skip(skip)
+        .limit(limit);
+
+        res.json({
+            data: feed
+        })
+
+    }
+    catch(error :any) {
+        res.status(400).send(error.message);
     }
 })
